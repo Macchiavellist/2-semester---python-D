256c7824dd36e759990503f40107b5a947fcce8d
kafein <e_abutalifuly@kbtu.kz>
2025-01-19 20:55:03 +0500

    papka dobavlena

--- string.py
+++
@@ -1,7 +0,0 @@
-x = str("era")
-y = str(2)
-z = str(3.0)
-print(x)
-print(y)
-print(z)
-print('hi' + " " + x)
--- syntax.py
+++
@@ -1,5 +0,0 @@
-#This is a comment.
-"""This is a
-multiline docstring."""
-print("Hello, World!")
-#это только пайтон все остальные работы будут в другом репозиторие
